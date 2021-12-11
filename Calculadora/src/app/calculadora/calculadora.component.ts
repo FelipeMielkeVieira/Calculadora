3640a04bc99c74cc0578328f17ae730e94fdc026
JuPalaczeski <felipe_mielke-vieira@estudante.sc.senai.br>
2021-12-10 21:20:25 -0300

    10/12

--- Calculadora/src/app/calculadora/calculadora.component.ts
+++ Calculadora/src/app/calculadora/calculadora.component.ts
@@ -15,12 +15,20 @@ export class CalculadoraComponent implements OnInit {
   ultimaResposta = '0';
 
   controle = '';
+  controleValores = '';
+  raiz = 0;
+  exp = 0;
 
   funcaoIgual() {
 
-    this.controle = eval(this.controle);
-    
-    if(this.controle == undefined) {
+    this.controle = (eval(this.controleValores)) + '';
+    this.controleValores = (eval(this.controleValores)) + '';
+
+    console.log(this.controle);
+    console.log(this.controleValores);
+
+    if (this.controleValores == undefined) {
+      this.controleValores = '';
       this.controle = '';
     }
 
@@ -31,25 +39,64 @@ export class CalculadoraComponent implements OnInit {
 
     evento = this.funcoesSecundarias(evento);
 
-    if(evento == '=') {
+    if(this.controle == '0') {
+      this.controle = '';
+      this.controleValores = '';
+    }
+
+    if (evento == '=') {
       this.funcaoIgual();
     } else {
       this.controle = this.controle + evento;
+      this.controleValores = this.controleValores + evento;
     }
   }
 
   funcoesSecundarias(a) {
 
-    if(a == 'DEL') {
-      this.controle = '';
+    if (this.raiz == 1 && a != "=" && a != 'DEL' && a != 'AC' && a != 'Ans' && a != 'EXP') {
+      this.controleValores = this.controleValores + a + ")";
+      this.controle = this.controle + a;
+      this.raiz = 0;
+      
       return '';
+    }
 
-    } else if(a == 'AC') {
+    if (this.exp == 1 && a != '=' && a != 'DEL' && a != 'AC' && a != 'Ans') {
+      this.exp = 0;
+      return a;
+    }
+
+    if (a == 'DEL') {
       this.controle = this.controle.slice(0, -1);
+      this.controleValores = this.controleValores.slice(0, -1);
       return '';
 
-    } else if(a == 'Ans') {
+    } else if (a == 'AC') {
+      this.controle = '';
+      this.controleValores = '';
+      return '';
+
+    } else if (a == 'Ans') {
       this.controle = this.controle + this.ultimaResposta;
+      this.controleValores = this.controleValores + this.ultimaResposta
+      return '';
+
+    } else if (a == '√') {
+      this.controleValores = this.controleValores + "Math.sqrt(";
+      this.controle = this.controle + "√";
+      this.raiz = 1;
+      return '';
+
+    } else if (a == "^") {
+      this.controle = this.controle + "^";
+      this.controleValores = this.controleValores + "**";
+      return '';
+
+    } else if (a == 'EXP') {
+      this.controle = this.controle + "E";
+      this.controleValores = this.controleValores + "10**";
+      this.exp = 1;
       return '';
 
     } else {
